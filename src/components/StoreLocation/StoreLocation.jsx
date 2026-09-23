2e7095f0c8c04fb31cea35ed6b95f015354bc7b4
nayanalshetty <nayanadl2006@gmail.com>
2026-09-23 18:42:03 +0530

    Use exact Google Maps embed for Viraj Jewellers

--- src/components/StoreLocation/StoreLocation.jsx
+++ src/components/StoreLocation/StoreLocation.jsx
@@ -8,17 +8,13 @@ import {
 import "./StoreLocation.css";
 
 const STORE_ADDRESS =
-  " M G Road, Viraj Jewellers, Opposite to Anjaneya Temple, 577101";
-
-// Exact Viraj Jewellers location
-const LATITUDE = 13.3191418;
-const LONGITUDE = 75.7741248;
+  "Opposite Anjaneya Temple, M G Road, Viraj Jewellers Shop, 577101";
 
 const GOOGLE_MAPS_URL =
-  `https://www.google.com/maps/search/?api=1&query=${LATITUDE},${LONGITUDE}`;
+  "https://www.google.com/maps/search/?api=1&query=Viraj%20Jewellers%2C%20Kadur%2C%20Karnataka%20577101";
 
 const MAP_EMBED_URL =
-  `https://www.google.com/maps?q=${LATITUDE},${LONGITUDE}&z=17&output=embed`;
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.5000345183325!2d75.7741248!3d13.3191418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbad796e2b8a86d%3A0x6b17a61bea19580!2sViraj%20Jewellers!5e0!3m2!1sen!2sin!4v1790169060729!5m2!1sen!2sin";
 
 export default function StoreLocation() {
   return (
@@ -54,7 +50,7 @@ export default function StoreLocation() {
               OUR STORE
             </span>
 
-            <h3>Viraj Jewels CKM</h3>
+            <h3>Viraj Jewellers</h3>
 
             <p className="store-location-address">
               {STORE_ADDRESS}
@@ -62,7 +58,6 @@ export default function StoreLocation() {
 
             <div className="store-location-info">
               <div className="store-location-info-row">
-
                 <Clock3 size={18} strokeWidth={1.5} />
 
                 <div>
@@ -72,7 +67,6 @@ export default function StoreLocation() {
                     Contact us for opening hours
                   </span>
                 </div>
-
               </div>
             </div>
 
@@ -94,18 +88,20 @@ export default function StoreLocation() {
           <div className="store-location-map">
 
             <iframe
-              title="Viraj Jewellers CKM Store Location"
+              title="Viraj Jewellers Store Location"
               src={MAP_EMBED_URL}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              allowFullScreen
+              width="600"
+              height="450"
               style={{
+                border: 0,
                 width: "100%",
                 height: "100%",
                 minHeight: "285px",
-                border: 0,
                 display: "block",
               }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="strict-origin-when-cross-origin"
             />
 
             <a
